c7509296951605cb4e515a91b84b3a18e6286078
Sigrid Gramlinger <gramlinger@webgras.at>
2021-12-07 21:01:53 +0100

    Update index.php
    We are testing

--- templates/cassiopeia/index.php
+++ templates/cassiopeia/index.php
@@ -3,8 +3,9 @@
  * @package     Joomla.Site
  * @subpackage  Templates.cassiopeia
  *
- * @copyright   (C) 2017 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright   (C) 2017 Open Source Matters, Inc. <https://www.joomla.org> 
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * Hi you
  */
 
 defined('_JEXEC') or die;
